001530dbaf81febbdf27bf1d484563cf333d9624
Kiprotich Dominic Korir <admin@factsafrica.com>
2020-09-12 23:30:17 +0300

    fix the blog sharing views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
@@ -33,14 +34,22 @@ def post_detail(request,year,month,day,post):
 
 def post_share(request,post_id):
     # Retrieve post by id
-    post = get_object_or_404(POst,id=post_id,status='published')
+    post = get_object_or_404(Post,id=post_id,status='published')
+    sent = False
     if request.method=='POST':
         # Form was submitted
-        form = EmailPostForm(request=POST)
+        form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'developappsafrica@gmail.com',
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request,'blog/post/share.html', {'post': post,'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,'form': form,'sent': sent})
