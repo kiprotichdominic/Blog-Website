f1461fc488b7e5e75653da9f263ffb3930170e04
Kiprotich Dominic Korir <admin@factsafrica.com>
2020-09-14 08:22:06 +0300

    add post display with similar posts

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,16 @@ from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post,Comment
 from .forms import EmailPostForm, CommentForm
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
 def post_list(request,tag_slug=None):
@@ -47,7 +48,11 @@ def post_detail(request,year,month,day,post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form})
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form,'similar_posts': similar_posts})
      
 
 def post_share(request,post_id):
